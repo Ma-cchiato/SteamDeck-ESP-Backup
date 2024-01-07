5dfc6fd965f76ae8466d8868deb95db47e3891ab
Ma-cchiato <macchiato.vrchat@gmail.com>
2024-01-07 13:07:49 +0900

    Apply English translation
    Thanks for the translation, evlav (https://gitlab.com/evlaV)

--- README.md
+++ README.md
@@ -1,58 +1,59 @@
-# ESP 관리 스크립트
+# ESP Management Script
 
-이 스크립트는 리눅스 시스템의 ESP(EFI System Partition)를 관리하기 위해 사용됩니다. 사용자는 이 스크립트를 통해 ESP를 백업하거나 복원할 수 있으며, 부팅 목록을 복구할 수 있습니다.
+ko [한국어](README.ko.md)
 
-## 주의사항
+This script is used to manage the ESP (EFI System Partition) of a Linux system. This script allows users to backup or restore ESP and recover boot list.
 
-- **면책 조항**: 이 스크립트는 '있는 그대로' 제공되며, 사용으로 인한 어떠한 손실이나 손상에 대해 책임을 지지 않습니다. 사용자는 스크립트 사용에 따른 모든 위험을 감수해야 하며, 중요한 데이터의 손실이나 시스템 손상의 가능성을 고려하여 신중히 사용해야 합니다.
-- 복원 기능을 사용할 때는 백업 파일과 Hash.txt 파일은 같은 디렉토리에 위치해있어야 합니다.
-- 부팅 목록 복구는 리커버리 모드가 아닌 일반 부팅시에만 사용이 가능합니다.
+## Caution
 
-## 기능
+- **Disclaimer**: This script is provided 'as is' and we will not be liable for any loss or damage resulting from its use. Users assume all risks associated with using the script, and should use it cautiously considering the possibility of loss of important data or damage to the system.
+- When using the restore function, the backup file and hash.txt file must be located in the same directory.
+- Boot list recovery can only be used during normal booting, not recovery mode.
 
-- **ESP 백업**: 현재 시스템의 ESP를 백업합니다.
-- **ESP 복원**: 백업된 ESP 파일을 복원합니다.
-- **부팅 목록 복구**: SteamOS 및 Windows 부팅 항목을 확인하고, 필요한 경우 부팅 목록을 복구하고 부팅 순서를 재설정합니다.
+## Function
 
-## 사용 방법
+- **ESP Backup**: Backs up the ESP of the current system.
+- **ESP Restore**: Restore backed up ESP files.
+- **Boot List Recovery**: Check SteamOS and Windows boot entries, repair boot list and reset boot order if necessary.
 
+## How to use
 
-1. **리커버리 모드 확인**: 스크립트 실행 시, 리커버리 모드에서 실행했는지를 묻는 대화 상자가 나타납니다. 리커버리 모드에서 실행했다면 'Yes'를 선택합니다.
+1. **Check recovery mode**: When running the script, a dialog box will appear asking whether it was run in recovery mode. If running in recovery mode, select 'Yes'.
 
    ![image](https://github.com/Ma-cchiato/SteamDeck-ESP-Backup/assets/122413511/be3871a8-caf8-4421-9249-27286b85f8f4)
 
-2. **sudo 비밀번호 입력**: 일반 모드에서 실행하는 경우, sudo 비밀번호 입력을 요구하는 대화 상자가 나타납니다. sudo 비밀번호를 입력하고 'OK'를 클릭합니다.
+2. **Enter your sudo password**: If running in normal mode, a dialog box will appear asking you to enter your sudo password. Enter your sudo password and click 'OK'.
 
    ![image](https://github.com/Ma-cchiato/SteamDeck-ESP-Backup/assets/122413511/5508db94-7657-4034-a73f-3b7cf4164822)
 
-3. **작업 선택**: Backup, Restore, Repair Boot Order 중 하나를 선택 후 'OK'를 클릭합니다. 선택한 작업에 따라 다음 단계가 진행됩니다.
+3. **Select task**: Select one of Backup, Restore, and Repair Boot Order and click 'OK'. The next steps will follow depending on the action you selected.
 
    ![image](https://github.com/Ma-cchiato/SteamDeck-ESP-Backup/assets/122413511/413e8ed4-7dfd-4b88-b40b-3c8f74faa9ad)
 
-- **Backup (ESP 백업)**
-  - 백업 선택 시, /esp 디렉토리의 모든 파일을 백업합니다.
-  - 기존에 백업 파일이 존재하는 경우 덮어쓸지 여부를 묻는 메시지가 표시됩니다. 'Yes'를 선택하면 기존 백업 파일이 삭제되고 새 백업 파일이 생성됩니다.
-  - 백업이 완료되면 완료 알림이 표시됩니다. 백업된 압축 파일과 압축파일명-hash.txt 파일이 생성됩니다.
+- **Backup (ESP Backup)**
+  - When you select Backup, all files in the /esp directory are backed up.
+  - If a backup file already exists, you will be prompted to overwrite it. If you select 'Yes', the existing backup file will be deleted and a new backup file will be created.
+  - When the backup is complete, you will see a completion notification. The backed up compressed file and compressed file name-hash.txt file are created.
 
     ![image](https://github.com/Ma-cchiato/SteamDeck-ESP-Backup/assets/122413511/528c0798-fdbc-4fcf-8bd8-f42a58efe6e1)
     ![image](https://github.com/Ma-cchiato/SteamDeck-ESP-Backup/assets/122413511/3746b1bb-f7bf-43a4-b09a-25df58211139)
 
-- **Restore (ESP 복원)**
-  - 복원을 선택하면, 백업 파일을 선택할 수 있는 대화 상자가 나타납니다.
-  - 백업 파일을 선택하면, 파일의 해시값을 확인하고 복원 프로세스가 시작됩니다.
-  - 복원이 완료되면 완료 알림이 표시됩니다.
+- **Restore (ESP Restore)**
+  - When you select Restore, a dialog box will appear allowing you to select the backup file.
+  - Once you select a backup file, the hash value of the file will be verified and the restoration process will begin.
+  - When the restore is complete, you will see a completion notification.
 
     ![image](https://github.com/Ma-cchiato/SteamDeck-ESP-Backup/assets/122413511/07f5c853-4b55-4cf9-9c88-4cbe37f63dc3)
 
-- **Repair Boot Order (부팅 목록 복구)**
-  - 부팅 목록 복구를 선택하면, 듀얼 부팅 여부를 묻는 메시지가 나타납니다.
-  - 듀얼 부팅인 경우, SteamOS와 Windows 부트 항목을 추가하고 순서를 설정합니다.
-  - 단일 OS인 경우, SteamOS 부트 항목만 추가합니다.
-  - 작업이 완료되면 완료 알림이 표시됩니다.
+- **Repair Boot Order (Boot List Recovery)**
+  - If you select Repair Boot List, you will be prompted to dual boot.
+  - If dual booting, add SteamOS and Windows boot entries and set their order.
+  - For a single OS, add only the SteamOS boot entry.
+  - When the task is complete, you will see a completion notification.
 
     ![image](https://github.com/Ma-cchiato/SteamDeck-ESP-Backup/assets/122413511/cf595afa-c993-441d-ad56-26e001fad52c)
     ![image](https://github.com/Ma-cchiato/SteamDeck-ESP-Backup/assets/122413511/70a869c4-c8d0-4d62-8ef7-ddcb829b27d2)
 
-## 라이선스
+## License
 
-이 스크립트는 MIT 라이선스 하에 배포됩니다.
+This script is released under the MIT License.
--- esp-backup.sh
+++ esp-backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# 리커버리 부팅 여부 확인
-zenity --question --title="리커버리 모드 확인" --text="리커버리 USB 진입 후 실행하셨습니까?" --width=300 --height=120
+# Check whether recovery is booting
+zenity --question --title="Check recovery mode" --text="Did you run it after entering the recovery USB?" --width=300 --height=120
 RECOVERY_MODE=$?
 
-# 경로 및 파일명 정의
+# Define path and file name
 TODAY=$(date +%F)
 BACKUP_DIR="/home/deck"
 BACKUP_FILE="Backup_ESP-${TODAY}.zip"
@@ -13,79 +13,79 @@ HASH_FILE="${BACKUP_PATH}-hash.txt"
 ESP_MOUNT="/esp"
 REPAIR_DIR="/repair"
 
-# 리커버리 모드일 경우
+# In recovery mode
 if [ $RECOVERY_MODE -eq 0 ]; then
     sudo steamos-readonly disable
     sudo mkdir -p "$REPAIR_DIR"
     sudo mount /dev/nvme0n1p1 "$REPAIR_DIR"
     ESP_MOUNT="/repair"
 else
-    # sudo 비밀번호 입력
-    SUDO_PASS=$(zenity --password --title="sudo 비밀번호 입력" --width=300 --height=120)
+    # enter sudo password
+    SUDO_PASS=$(zenity --password --title="enter sudo password" --width=300 --height=120)
     if [ -z "$SUDO_PASS" ]; then
-        zenity --error --text="sudo 비밀번호가 필요합니다." --width=300 --height=120
+        zenity --error --text="A sudo password is required." --width=300 --height=120
         exit 1
     fi
 fi
 
-# 비밀번호 유효성 검사
+# Password validation
 echo $SUDO_PASS | sudo -S ls / &> /dev/null
 if [ $? -ne 0 ]; then
-    zenity --error --text="sudo 비밀번호가 잘못되었습니다." --width=300 --height=120
+    zenity --error --text="sudo password is incorrect." --width=300 --height=120
     exit 1
 fi
 
 backup_esp() {
     if [ -f "$BACKUP_PATH" ]; then
-        if zenity --question --text="백업 파일 ($BACKUP_FILE) 이 \n이미 존재합니다. 덮어쓰시겠습니까?" --width=300 --height=120; then
-            # 기존 백업 파일 및 Hash 삭제
+        if zenity --question --text="backup file ($BACKUP_FILE) This \nalready exists. Do you want to overwrite?" --width=300 --height=120; then
+            # Delete existing backup files and Hash
             sudo rm "$BACKUP_PATH"
             sudo rm "$HASH_FILE"
         else
-            zenity --info --text="백업을 취소했습니다." --width=300 --height=120
+            zenity --info --text="Backup has been cancelled." --width=300 --height=120
             exit 0
         fi
     fi
 
-    # 백업 파일 생성
+    # Create backup file
     if echo $SUDO_PASS | sudo -S zip -r "$BACKUP_PATH" "$ESP_MOUNT"; then
         echo $SUDO_PASS | sudo -S sha256sum "$BACKUP_PATH" > "$HASH_FILE"
-        zenity --info --text="백업이 완료되었습니다. \n$BACKUP_PATH" --width=300 --height=120
+        zenity --info --text="Backup is complete. \n$BACKUP_PATH" --width=300 --height=120
     else
-        zenity --error --text="백업에 실패했습니다." --width=300 --height=120
+        zenity --error --text="Backup failed." --width=300 --height=120
         exit 1
     fi
 }
 
-# 복원 함수
+# Restore function
 restore_esp() {
-    local restore_file=$(zenity --file-selection --title="복원할 ESP 백업 파일 선택" --width=300 --height=120)
+    local restore_file=$(zenity --file-selection --title="Select ESP backup file to restore" --width=300 --height=120)
     
-    # 사용자가 파일을 선택하지 않았을 경우
+    # If the user did not select a file
     if [ -z "$restore_file" ]; then
-        zenity --error --text="복원할 파일을 선택하지 않았습니다." --width=300 --height=120
+        zenity --error --text="You have not selected any files to restore." --width=300 --height=120
         exit 1
     fi
 
     local restore_hash_file="${restore_file}-hash.txt"
     
-    # 해시값 확인
+    # Check hash value
     if [ -f "$restore_hash_file" ]; then
         local file_hash=$(sha256sum "$restore_file" | awk '{ print $1 }')
-        local saved_hash=$(cat "$restore_hash_file" | awk '{ print $1 }')
+        local saved_hash=$(awk '{ print $1 }' "$restore_hash_file")
         if [ "$file_hash" == "$saved_hash" ]; then
             if echo $SUDO_PASS | sudo -S unzip -o "$restore_file" -d "/"; then
-                zenity --info --text="복원이 완료되었습니다." --width=300 --height=120
+                zenity --info --text="Restoration is complete." --width=300 --height=120
             else
-                zenity --error --text="복원에 실패했습니다." --width=300 --height=120
+                zenity --error --text="Restore failed." --width=300 --height=120
                 exit 1
             fi
         else
-            zenity --error --text="해시 값이 일치하지 않습니다. 파일이 변조되었을 수 있습니다." --width=300 --height=120
+            zenity --error --text="Hash values do not match. The file may have been tampered with." --width=300 --height=120
             exit 1
         fi
     else
-        zenity --error --text="해시 파일이 없습니다. 올바른 백업 파일을 선택했는지 확인하세요." --width=300 --height=120
+        zenity --error --text="There is no hash file. Make sure you select the correct backup file." --width=300 --height=120
         exit 1
     fi
 }
@@ -96,62 +96,62 @@ repair_boot_order() {
     local steamos_entry_number=$(echo "$steamos_entry" | awk '{print $1}' | sed 's/Boot//g' | sed 's/\*//g')
     local windows_entry_number=$(echo "$windows_entry" | awk '{print $1}' | sed 's/Boot//g' | sed 's/\*//g')
     if [ $RECOVERY_MODE -eq 0 ]; then
-        zenity --error --text="리커버리에서는 진행할 수 없습니다.\nSteamOS 부팅 후 진행해주세요." --width=300 --height=120
+        zenity --error --text="You cannot proceed in recovery.\nPlease proceed after booting SteamOS." --width=300 --height=120
         exit 1
     else
-    # 듀얼 부팅 여부 확인
-    if zenity --question --text="듀얼 부팅을 사용하고 있습니까?" --width=300 --height=120; then
-        # 이미 부트 항목이 존재하는 경우 부팅 순서가 올바른지 확인
-        if [ ! -z "$windows_entry" ] && [ ! -z "$steamos_entry" ]; then
+    # Check if you have dual boot
+    if zenity --question --text="Are you using dual boot?" --width=300 --height=120; then
+        # If a boot entry already exists, ensure the boot order is correct
+        if [ -n "$windows_entry" ] && [ -n "$steamos_entry" ]; then
             local current_order=$(echo $SUDO_PASS | sudo -S efibootmgr | grep "^BootOrder" | cut -d ':' -f 2)
             if [[ $current_order == *"$steamos_entry_number"* && $current_order == *"$windows_entry_number"* ]]; then
                 if [[ $current_order != *"$steamos_entry_number"*"$windows_entry_number"* ]]; then
-                    # SteamOS를 우선 순위로 설정
+                    # Set SteamOS as priority
                     echo $SUDO_PASS | sudo -S efibootmgr --bootorder "$steamos_entry_number,$windows_entry_number"
                 else
-                    zenity --info --text="부팅 목록이 이미 정상적으로 존재합니다." --width=300 --height=120
+                    zenity --info --text="The boot list already exists normally." --width=300 --height=120
                     return
                 fi
             fi
         fi
         
-        # SteamOS 부트 항목 추가
+        # Add SteamOS boot entry
         if [ -z "$steamos_entry" ]; then
             #echo $SUDO_PASS | sudo -S efibootmgr -c -L "SteamOS" -l "\efi\steamos\steamcl.efi" -d /dev/nvme0n1p1
             steamos_entry_number=$(echo $SUDO_PASS | sudo -S efibootmgr --create --disk /dev/nvme0n1 --part 1 --label "SteamOS" --loader "\EFI\steamos\steamcl.efi" | grep 'BootOrder' | awk '{print $2}')
         fi
 
-        # Windows 부트 항목 추가
+        # Add Windows boot entry
         if [ -z "$windows_entry" ]; then
             #echo $SUDO_PASS | sudo -S efibootmgr -c -L "Windows Boot Manager" -l "\efi\Microsoft\Boot\bootmgfw.efi" -d /dev/nvme0n1p1
             windows_entry_number=$(echo $SUDO_PASS | sudo -S efibootmgr --create --disk /dev/nvme0n1 --part 1 --label "Windows Boot Manager" --loader "\EFI\Microsoft\Boot\bootmgfw.efi" | grep 'BootOrder' | awk '{print $2}')
         fi
 
-        # 부트 순서 변경
+        # Change boot order
         echo $SUDO_PASS | sudo -S efibootmgr --bootorder "$steamos_entry_number,$windows_entry_number"
     else
-        # 단일 OS이지만 이미 부트 항목이 존재하는 경우
-        if [ ! -z "$steamos_entry" ]; then
-            zenity --info --text="부팅 목록이 이미 정상적으로 존재합니다." --width=300 --height=120
+        # Single OS but boot entry already exists
+        if [ -n "$steamos_entry" ]; then
+            zenity --info --text="The boot list already exists normally." --width=300 --height=120
             return
         fi
 
-        # SteamOS 부트 항목 추가
+        # Add SteamOS boot entry
         if [ -z "$steamos_entry" ]; then
             echo $SUDO_PASS | sudo -S efibootmgr --create --disk /dev/nvme0n1 --part 1 --label "SteamOS" --loader "\EFI\steamos\steamcl.efi"
         fi
     fi
     fi
 
-    zenity --info --text="부팅 순서가 업데이트되었습니다." --width=300 --height=120
+    zenity --info --text="Boot order has been updated." --width=300 --height=120
 }
 
 
 
-# 작업 선택
-choice=$(zenity --list --title="ESP 관리" --column="작업 선택" "Backup" "Restore" "Repair Boot Order" --width=300 --height=220 --hide-header)
+# Select task
+choice=$(zenity --list --title="ESP Management" --column="Select task" "Backup" "Restore" "Repair Boot Order" --width=300 --height=220 --hide-header)
 
-# 선택된 작업에 따라 함수 실행
+# Execute functions based on selected actions
 case $choice in
     "Backup")
         backup_esp
@@ -163,6 +163,6 @@ case $choice in
         repair_boot_order
         ;;
     *)
-        zenity --error --text="올바른 옵션을 선택하지 않았습니다." --width=300 --height=120
+        zenity --error --text="You did not select a correct option." --width=300 --height=120
         ;;
 esac
